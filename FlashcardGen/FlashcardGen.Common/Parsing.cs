547ebde815de0065e5c23eba0cd9f68845bc9572
Kenton Huff <kentonahuff@gmail.com>
2024-06-29 01:54:47 -0500

    Parsing

--- FlashcardGen/FlashcardGen.Common/Parsing.cs
+++ FlashcardGen/FlashcardGen.Common/Parsing.cs
@@ -6,7 +6,49 @@ namespace FlashcardGen.Common
     {
         public static OpenGNTRowEntities ParseOpenGNTRow(string openGNTRow)
         {
-            return new();
+            openGNTRow = openGNTRow.Replace("〕", "");
+
+            string[] firstSplit = openGNTRow.Split("\t〔");
+
+            string[] verseNumbers = firstSplit[2].Split('｜');
+
+            OpenGNTRowEntities result = new();
+            result.Verse = new()
+            {
+                BookNumber = Int32.Parse(verseNumbers[0]),
+                ChapterNumber = Int32.Parse(verseNumbers[1]),
+                VerseNumber = Int32.Parse(verseNumbers[2]),
+            };
+
+            string[] greekForms = firstSplit[3].Split('｜');
+            string[] englishForms = firstSplit[6].Split('｜');
+            string[] punctuationMarks = firstSplit[7].Split('｜');
+
+            result.Lexeme = new()
+            {
+                ExtendedStrongsNumber = greekForms[5],
+                TyndaleHouseGloss = englishForms[0],
+                LexicalForm = greekForms[3],
+            };
+
+            result.WordForm = new()
+            {
+                LowercaseSpelling = greekForms[2].ToLowerInvariant(),
+                RobinsonsMorphologicalAnalysisCode = greekForms[4],
+            };
+
+            result.WordFormOccurrence = new()
+            {
+                IsCapitalized = char.IsUpper(greekForms[2][0]),
+                StudyTranslationGloss = englishForms[3],
+                PreOccurrencePunctuationMarks = punctuationMarks[0].Replace("<pm>", "").Replace("</pm>", ""),
+                PostOccurrencePunctuationMarks = punctuationMarks[1].Replace("<pm>", "").Replace("</pm>", ""),
+            };
+
+            if (greekForms[2].Length > 1 && char.IsUpper(greekForms[2][1]))
+                Console.WriteLine($"Word with multiple capitalized characters: {greekForms[2]}");
+
+            return result;
         }
     }
 }
--- FlashcardGen/FlashcardGen.Models/DbModels/WordFormOccurrence.cs
+++ FlashcardGen/FlashcardGen.Models/DbModels/WordFormOccurrence.cs
@@ -5,7 +5,7 @@
         public int WordFormOccurrenceId { get; set; }
 
         public bool IsCapitalized { get; set; }
-        public int PositionInVerse { get; set; }
+        //public int PositionInVerse { get; set; }
         public string StudyTranslationGloss { get; set; }
         public string PreOccurrencePunctuationMarks { get; set; }
         public string PostOccurrencePunctuationMarks { get; set; }
